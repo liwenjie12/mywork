b85bb8884e27b49a074f5d2af48c86bf23b47cd5
liwenjie12 <1499967138@qq.com>
2019-01-11 00:14:39 +0800

    携程弹出条

---
+++ xiecheng/js/tanchutiao.js
@@ -0,0 +1,17 @@
+var leftLogo=document.getElementsByClassName("leftlogo")[0];
+var leftFixed=document.getElementsByClassName("leftfixed")[0];
+var closeBtn=document.getElementsByClassName("close")[0];
+leftLogo.addEventListener("click",function (){
+    leftLogo.style.left="-100%";
+    leftLogo.style.transition="left 1s ease";
+    leftFixed.style.left="0%";
+    leftFixed.style.transition = "left 2s ease";
+});
+closeBtn.addEventListener("click",function (){
+    leftFixed.style.left="-100%";
+    leftFixed.style.transition="left 2s ease";
+    leftFixed.style.mstransition="left 2s ease";
+    leftLogo.style.left="0%";
+    leftLogo.style.transition="left 3s ease";
+    //leftLogo.style.-ms-transition="left 3s ease";
+});
